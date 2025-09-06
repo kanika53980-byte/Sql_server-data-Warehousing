77962bbfcc23dd63b87befee0141096eaf343b7a
kanika53980-byte <kanika53980@gmail.com>
2025-09-06 17:14:13 +0530

    Update init_database-.sql

--- Scripts/init_database-.sql
+++ Scripts/init_database-.sql
@@ -1,26 +1,42 @@
 /*
-=================================================================================== 
-Create Database and Schemas 
-===================================================================================
-Script Purose:
-     This script creates a new database named 'DataWarehouse' after checking if it
-     already exists, it is dropped and recreated. Additionally, the script sets up
-     three tables within the database: 'broze', 'silver', and 'gold'.
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three
+    schemas within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution and 
+    ensure you have proper backups before running this script.
+*/
 
-Warning:
-     Running this script will drop the entire 'DataWarehouse' database if it exists.
-     All data in the database will be permanently deleted. Proceed with caution and 
-     ensure you have proper backups before running this script.
-*/ 
-USE mysql;
+USE master;
+GO
 
 -- Drop and recreate the 'DataWarehouse' database
-     
-DROP DATABASE IF EXISTS DataWarehouse;
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
 
--- create the 'DataWarehouse' database
+-- Create the 'DataWarehouse' database
 CREATE DATABASE DataWarehouse;
-Use Datawarehouse;
-CREATE TABLE bronze;
-CREATE TABLE silver;
-CREATE TABLE gold;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
